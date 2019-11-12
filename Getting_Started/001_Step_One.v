671d80ad9049ad81f6c7041801f13b0280c39250
zhangcheng <czhang223@163.com>
2019-11-13 00:04:23 +0800

    ./Procedures/034_Always_case2 submit

--- Getting_Started/001_Step_One.v
+++ Getting_Started/001_Step_One.v
@@ -3,5 +3,6 @@ module top_module( output one );
 // Insert your code here
     wire x;
     assign one = 1'b1;
+    ddasa
 
 endmodule
--- Verilog_Language/Basics/003_Wire.v
+++ Verilog_Language/Basics/003_Wire.v
@@ -1,5 +1,4 @@
 module top_module( input in, output out );
     
     assign out = in;
-
 endmodule
